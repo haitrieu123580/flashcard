46019c2b58107c934a45200e0139554e5d8904a8
trieu <haitrieu123580@gmail.com>
2024-06-12 01:24:03 +0700

    fix: import file

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -1,52 +1,46 @@
 import 'reflect-metadata';
-// prettier-ignore
+// import 'module-alias/register';
 import moduleAlias from 'module-alias';
-// prettier-ignore
 moduleAlias.addAliases({
   "@src": `${__dirname}/`,
   "@routers": `${__dirname}/routers`,
   "@controllers": `${__dirname}/controllers`,
-  "@s ervices": `${__dirname}/services`,
+  "@services": `${__dirname}/services`,
   "@repositories": `${__dirname}/repositories`,
   "@middleware": `${__dirname}/middleware`,
   "@entity": `${__dirname}/entity`,
   "@dto": `${__dirname}/dto`,
   "@helper": `${__dirname}/helper`,
 });
-
-
-import cors from 'cors';
+import express, { Request, Response, NextFunction, Application } from 'express';
 import dotenv from 'dotenv';
-import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import './services/oauth/Passport';
-
-import fs from 'fs';
-import path from 'path';
-import session from 'express-session';
-import passport from 'passport';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yaml';
-import adminAprroveUserSetRouter from '@routers/approve-sets/index';
+import fs from 'fs';
+import path from 'path';
+import passport from 'passport';
+import session from 'express-session';
+import { AppDataSource } from "./data-source"
 import authRoutes from '@routers/auth/index';
-import cardRouter from '@routers/card/index';
-import passportRouter from '@routers/passport/index';
-import testKitRouter from '@routers/test-kit/index';
-import testRouter from '@routers/test-sets/index';
-import userCardsRouter from '@routers/user-cards/index';
-import userProgressRouter from '@routers/user-progress/index';
-import userSetsRouter from '@routers/user-sets/index';
-import userTestRouter from '@routers/user-tests/index';
 import userRouter from '@routers/user/index';
+import passportRouter from '@routers/passport/index';
 import vocabRouter from '@routers/vocabulary-set/index';
-
+import cardRouter from '@routers/card/index';
+import userSetsRouter from '@routers/user-sets/index';
+import userCardsRouter from '@routers/user-cards/index';
+import testRouter from "@routers/test-sets/index";
+import userTestRouter from "@routers/user-tests/index";
+import userProgressRouter from "@routers/user-progress/index";
+import adminAprroveUserSetRouter from "@routers/approve-sets/index";
+import testKitRouter from '@routers/test-kit/index';
 import {
-  ApiError,
-  ErrorType,
-  InternalError,
   NotFoundError,
+  ApiError,
+  InternalError,
+  ErrorType,
 } from './core/ApiError';
-import { AppDataSource } from './data-source';
-
 dotenv.config();
 
 const app: Application = express();
@@ -55,31 +49,32 @@ const app: Application = express();
 const connectToDatabase = async () => {
   try {
     await AppDataSource.initialize();
-    console.log('Database connection established successfully.');
+    console.log("Database connection established successfully.");
   } catch (error) {
     console.log(error);
-    console.log('Retrying connection in 5 seconds...');
+    console.log("Retrying connection in 5 seconds...");
     setTimeout(connectToDatabase, 5000);
   }
 };
 
 connectToDatabase();
 
+
 // Swagger
 try {
-  const file = fs.readFileSync(
-    path.resolve(__dirname, '../docs/swagger.yaml'),
-    'utf8',
-  );
+  const file = fs.readFileSync(path.resolve(__dirname, '../docs/swagger.yaml'), 'utf8')
   const swaggerDocument = YAML.parse(file);
-  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-} catch (error) { }
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+} catch (error) {
+
+}
 
 //cors
 const corsOptions = {
   origin: String(process.env.CLIENT_URL),
   optionsSuccessStatus: 200,
   credentials: true,
+
 };
 app.use(cors(corsOptions));
 
@@ -87,27 +82,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Passport
-app.use(
-  session({
-    secret: String(process.env.SESSION_KEY),
-    resave: false,
-    saveUninitialized: true,
-  }),
-);
+app.use(session({
+  secret: String(process.env.SESSION_KEY),
+  resave: false,
+  saveUninitialized: true,
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRouter);
-app.use('/passport', passportRouter);
-app.use('/api/vocabulary-set', vocabRouter);
-app.use('/api/card', cardRouter);
-app.use('/api/user-sets', userSetsRouter);
-app.use('/api/user-cards', userCardsRouter);
-app.use('/api/tests', testRouter);
-app.use('/api/user-tests', userTestRouter);
-app.use('/api/user-progress', userProgressRouter);
-app.use('/api/admin/approve-set', adminAprroveUserSetRouter);
-app.use('/api/test-kit', testKitRouter);
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRouter)
+app.use('/passport', passportRouter)
+app.use('/api/vocabulary-set', vocabRouter)
+app.use('/api/card', cardRouter)
+app.use('/api/user-sets', userSetsRouter)
+app.use('/api/user-cards', userCardsRouter)
+app.use('/api/tests', testRouter)
+app.use('/api/user-tests', userTestRouter)
+app.use('/api/user-progress', userProgressRouter)
+app.use('/api/admin/approve-set', adminAprroveUserSetRouter)
+app.use('/api/test-kit', testKitRouter)
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(new NotFoundError()));
 
@@ -115,7 +108,7 @@ app.use((req, res, next) => next(new NotFoundError()));
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ApiError) {
     ApiError.handle(err, res);
-    console.log('error', err);
+    console.log("error", err)
     // if (err.type === ErrorType.INTERNAL)
     // Logger.error(
     //     `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
@@ -129,16 +122,14 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     //     return res.status(500).send(err);
     // }
     ApiError.handle(new InternalError(), res);
-    console.log('error', err);
+    console.log("error", err)
   }
 });
 
 const port = process.env.PORT || 8000;
-app.listen(Number(port), '0.0.0.0', () => {
-  console.log(
-    `server is running on http://localhost:${process.env.PORT || 8000}`,
-  );
-});
+app.listen(Number(port), "0.0.0.0", () => {
+  console.log(`server is running on http://localhost:${process.env.PORT || 8000}`)
+})
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
