3d0c2b75b88ca0c6dbb14b381bb2986d63e9ef45
trieu <haitrieu123580@gmail.com>
2024-06-17 10:19:01 +0700

    feat: firebase upload

---
+++ back_end/src/core/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7OYiC3_52v5NOpZIardAqBjiDshFjVrY",
+    authDomain: "uploading-ce62a.firebaseapp.com",
+    projectId: "uploading-ce62a",
+    storageBucket: "uploading-ce62a.appspot.com",
+    messagingSenderId: "18233155661",
+    appId: "1:18233155661:web:134265c8987c8c3c363c04",
+    measurementId: "G-7YHJW7SKBH"
+};
